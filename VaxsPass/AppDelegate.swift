01af097f406798178eef14bf8309a27a65a7182c
Nada Zeini <nada.elzeini@sjsu.edu>
2021-02-13 13:04:34 -0800

    Modified project and AppDelegate

--- VaxsPass/AppDelegate.swift
+++ VaxsPass/AppDelegate.swift
@@ -12,16 +12,12 @@ import GooglePlaces
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         AGSArcGISRuntimeEnvironment.apiKey = "AAPK175781fbc4044b99bdd35a4347bde45deU7VJt25W0DC6qG3IzlX5lA5rmj7sC2ABbqcgdaA3iAF84u66vrhW6Wc4nt7KUXd"
         GMSServices.provideAPIKey("AIzaSyARNJeV0HUI1CcAVuKN6VQ__PwET2KZ6Rc")
         GMSPlacesClient.provideAPIKey("AIzaSyARNJeV0HUI1CcAVuKN6VQ__PwET2KZ6Rc")
         return true
     }
-
-
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
     
@@ -31,7 +27,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     
     }
-
-
 }
 

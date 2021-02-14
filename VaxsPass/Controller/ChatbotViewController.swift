84767231b06767306d07c7c026b088da0b13389a
Nikita Petrenko <thenikitapetrenko@gmail.com>
2021-02-14 11:25:55 -0500

    Add chatbot logic

--- VaxsPass/Controller/ChatbotViewController.swift
+++ VaxsPass/Controller/ChatbotViewController.swift
@@ -6,46 +6,28 @@
 //
 
 import UIKit
-import ApiAI
 import AVFoundation
+import Alamofire
 
 class ChatbotViewController: UIViewController {
+    
+    var query: [String: AnyObject] = ["query": ""]
 
     @IBOutlet weak var responseField: UILabel!
     @IBOutlet weak var textField: UITextField!
     @IBAction func askButton(_ sender: Any) {
-        
-        let request = ApiAI.shared().textRequest()
-        
-        if let text = self.textField.text, text != "" {
-            request?.query = text
-        } else {
-            return
+        query["query"] = textField.text
+        Alamofire.request("https://glacial-inlet-64915.herokuapp.com/openai", method: .post, parameters: query).responseJSON { response in
+            print(response.request)   // original url request
+            print(response.response) // http url response
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
         }
         
-        request?.setMappedCompletionBlockSuccess({ (request, response) in
-            let response = response as! AIResponse
-            if let textResponse = response.result.fulfillment.speech {
-                self.speechAndText(text: textResponse)
-            }
-            
-        }, failure: { (request, error) in
-            print(error!)
-        })
-        
-        ApiAI.shared().enqueue(request)
-        textField.text = ""
     }
     
-    let speechSynthesizer = AVSpeechSynthesizer()
     
-    func speechAndText(text: String) {
-        let speechUtterance = AVSpeechUtterance(string: text)
-        speechSynthesizer.speak(speechUtterance)
-        UIView.animate(withDuration: 1.0, delay: 0.0, options: .curveEaseInOut, animations: {
-            self.responseField.text = text
-        }, completion: nil)
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()

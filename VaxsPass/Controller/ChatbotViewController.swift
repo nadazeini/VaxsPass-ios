e8164c4614c3e28c68fd42a8f7333db58e4b4370
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2021-02-15 00:11:18 +0530

    Update Chatbot UI

--- VaxsPass/Controller/ChatbotViewController.swift
+++ VaxsPass/Controller/ChatbotViewController.swift
@@ -12,10 +12,19 @@ import Alamofire
 class ChatbotViewController: UIViewController {
     
     var query: Parameters = ["query": ""]
-
+    let alert = UIAlertController(title: nil, message: "Please wait...", preferredStyle: .alert)
+    
     @IBOutlet weak var responseField: UILabel!
     @IBOutlet weak var textField: UITextField!
     @IBAction func askButton(_ sender: Any) {
+        
+        let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
+        loadingIndicator.hidesWhenStopped = true
+        loadingIndicator.style = UIActivityIndicatorView.Style.large
+        loadingIndicator.startAnimating();
+        alert.view.addSubview(loadingIndicator)
+        present(alert, animated: true, completion: nil)
+        
         if let safeText = textField.text{
             query["query"] = safeText
             AF.request("https://glacial-inlet-64915.herokuapp.com/openai",method: .post,parameters: query,encoding: JSONEncoding.default, headers: .none).responseData(completionHandler: { (response) in
@@ -24,6 +33,7 @@ class ChatbotViewController: UIViewController {
                     self.responseField.text = decodedData?.response
                     
                 }
+                self.alert.dismiss(animated: true, completion: nil)
                 }
             )
             

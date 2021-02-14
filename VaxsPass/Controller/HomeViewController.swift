c143ca9cf14ede7ba6803efc82348b53e1be5e72
Nada Zeini <nada.elzeini@sjsu.edu>
2021-02-13 22:45:20 -0800

    QR code generating

--- VaxsPass/Controller/HomeViewController.swift
+++ VaxsPass/Controller/HomeViewController.swift
@@ -11,25 +11,33 @@ import Alamofire
 
 class HomeViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     
+    @IBOutlet weak var imgQRCode: UIImageView!
     @IBOutlet weak var GenerateQRButton: UIButton!
+    var qrcodeImage: CIImage!
     let create_user_params : [String:Any] = [
-            "oauth": "testing",
-            "name": "test",
-            "date" : "01/01/2021",
-            "vaccine_type" : "None",
-            "taken": 0,
-            "completed": false
+        "oauth": "testing123",
+        "name": "test123",
+        "date" : "01/01/2021",
+        "vaccine_type" : "None",
+        "taken": 1,
+        "completed": false
     ]
     @IBAction func GenerateQRCode(_ sender: Any) {
         AF.request("https://glacial-inlet-64915.herokuapp.com/create-user", method: .post, parameters: create_user_params, encoding: JSONEncoding.default)
             .responseJSON { response in
                 print(response)
             }
+        let passUrl = "https://glacial-inlet-64915.herokuapp.com/index.html?oauth=nada"
+        let stringData = passUrl.data(using: .utf8)
+        if qrcodeImage == nil {
+            let filter = CIFilter(name: "CIQRCodeGenerator")
+            filter?.setValue(stringData, forKey: "inputMessage")
+            filter?.setValue("Q", forKey: "inputCorrectionLevel")
+            qrcodeImage = filter?.outputImage
+            imgQRCode.image = UIImage(ciImage: qrcodeImage)
+        }
     }
     let image = UIImagePickerController()
-    
-    
-    
     let headers: HTTPHeaders = [
         "Authorization": "Bearer AIzaSyBZZOFM9otkX6J0NsDtDoNyNybY3HG7xeE",
         "Content-Type": "application/json; charset=utf-8"
